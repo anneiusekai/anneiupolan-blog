df60207ea2a48784ee26bb6f2ee0d0ede4ca7b05
Po-An Chen <r06521541@ntu.edu.tw>
2026-03-23 21:45:42 +0800

    Initial commit: Blog ê基礎建設佮 Cloudfare R2 圖片ê連結

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,10 @@ import { defineConfig } from 'astro/config';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
+	image: {
+		// 把你的 R2.dev 網址貼在這裡（不要包含 https:// 和後面的路徑）
+		domains: ['pub-7e355146c9504fa4af57be0afd2a07ea.r2.dev'], 
+	}
 });
+
+
